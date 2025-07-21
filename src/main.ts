aad371fd884c400cd5260755f0631b5b70bc73b5
Pedro Henrique <pedrohaugusto12@gmail.com>
2025-07-21 00:38:42 -0300

    build: Adicionando pipe global para garantir validação com Joi

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,14 @@ import { AppModule } from './app.module';
 import * as swaggerJsdoc from 'swagger-jsdoc';
 import * as swaggerUi from "swagger-ui-express";
 import { swaggerConfig } from "./swagger";
+import { JoiPipe  } from 'nestjs-joi';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const specs = swaggerJsdoc(swaggerConfig);
   app.use("/swagger", swaggerUi.serve, swaggerUi.setup(specs));
+  app.useGlobalPipes(new JoiPipe());
 
   await app.listen(process.env.PORT ?? 3000);
 }
